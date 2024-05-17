16fb67ce7cc112d34231acfffb626a3bc9d4620d
SwifferSK <theas.damien@gmail.com>
2024-05-17 08:20:20 +0200

    https://github.com/SwifferSK/Projet-sites-bachelor.git

--- django_project/bachelor_projects/models.py
+++ django_project/bachelor_projects/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Project(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text_descriptipo = models.TextField()
+    text_description = models.TextField()
     text_explication = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     #published_date = models.DateTimeField(blank=True, null=True)
